899371807687c911e269c7bcad4f467d112bae30
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2023-06-25 09:31:34 +0300

    added homework 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,56 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+
+const items = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+}).join('');
+
+gallery.innerHTML = items;
+
+let instance;
+
+
+const onImageClickOpen = function (event) {
+  const source = event.target.dataset.source
+  event.preventDefault()
+  
+
+
+  if (!(event.target.nodeName === "IMG")) {
+    return
+  }
+  instance = basicLightbox.create(`<img src="${source}" width="800" height="600">`)
+  console.log(event.target.dataset)
+  
+  instance.show()
+
+
+}
+
+const onImageClickClose = function (event) {
+    if (event.code === "Escape") {
+      instance.close()
+    }
+}
+
+window.addEventListener("keydown", event => onImageClickClose(event))
+
+window.removeEventListener("keydown", event => onImageClickClose(event))
+
+gallery.addEventListener("click", event => onImageClickOpen(event))
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+
+const items = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+}).join('');
+
+gallery.innerHTML = items;
+
+new SimpleLightbox('.gallery a', { captionData: "alt", captionDelay: 250, });
+
+gallery.addEventListener("click", clickPhoto)
+
+const clickPhoto = function (event) {
+
+    if (!(event.target.nodeName === "IMG")) {
+        return
+    }
+
+    event.preventDefault()
+
+
+
+}
+
+
